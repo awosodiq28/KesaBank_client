73527c7d33177903b1f2f992141b3f2163159239
NaijasonEzeiru <jayseehe1035@gmail.com>
2024-09-08 11:27:25 +0100

    added: shadcn UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Hero from '@/components/Hero';
-import About from '@/components/About';
-import DepositCard from '@/components/DepositCard';
-import Services from '@/components/Services';
-import Banner from '@/components/Banner';
-import LoanCard from '@/components/LoanCard';
-import ContactForm from '@/components/ContactForm';
-import Spinner from '@/components/Spinner';
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import DepositCard from "@/components/DepositCard";
+import Services from "@/components/Services";
+import Banner from "@/components/Banner";
+import LoanCard from "@/components/LoanCard";
+import ContactForm from "@/components/ContactForm";
+import Spinner from "@/components/Spinner";
 
 export default async function Home() {
   return (
@@ -17,8 +17,8 @@ export default async function Home() {
       <Banner />
       <Services />
       <ContactForm />
-      <h1>We served over 500+ Customers</h1>
-      <div className='line'></div>
+      <h1 className="mt-6 text-4xl mb-3">We served over 500+ Customers</h1>
+      <div className="line"></div>
     </>
   );
 }
